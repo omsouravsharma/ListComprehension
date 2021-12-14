265d699f7fb5a0c9f9cbcd663921849a6534d6c1
Sourav Sharma <souravsharma.sep22@gmail.com>
2021-12-14 14:34:51 +0530

    Added exception handling

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ file = pd.read_csv("nato_phonetic_alphabet.csv")
 file_nato = pd.DataFrame(file)
 print(file_nato)
 
-# TODO: Create a dictionary
+
 dict_nato = {
     row.letter: row.code for (index, row) in file_nato.iterrows()
 }
 print(dict_nato)
+is_on = True
+while is_on:
+    try:
+        user_input = input("Please enter your name: ").upper()
+        list_comprehension = [dict_nato[i] for i in user_input]
 
-# TODO: Create a list of phonetic code word from user input
-
-user_input = input("Please enter your name: ").upper()
-
-
-list_comprehension = [dict_nato[i] for i in user_input]
-print(list_comprehension)
+    except KeyError:
+        print("Sorry, Only Letter in the alphabet please")
+    else:
+        print(list_comprehension)
+        is_on = False
